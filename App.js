09e50aa2590aa010e6cd36445359b9299597913d
Helton Denis <heltondeniss@gmail.com>
2018-06-20 20:32:24 -0300

    Feature add insert itens

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, TextInput, Button} from 'react-native';
 import Item from './src/item';
 
 export default class TodoOnline extends Component {
@@ -7,22 +7,68 @@ export default class TodoOnline extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        lista:[]
+        lista:[],
+        input:''
     };
 
-    // Buscando o WebService //
-    fetch('https://b7web.com.br/todo/33431')
+    this.url = 'https://b7web.com.br/todo/33431';
+    this.loadLista = this.loadLista.bind(this);
+    this.addButton = this.addButton.bind(this);
+
+    this.loadLista();
+
+  }
+
+  loadLista(){
+
+  	 // Buscando o WebService //
+    fetch(this.url)
       .then((r)=>r.json())
       .then((json)=>{
         let state = this.state;
         state.lista = json.todo;
         this.setState(state);
       });
+
   }
+
+  addButton(){
+  	let texto = this.state.input;
+  	let state = this.state;
+
+  	state.input = '',
+  	this.setState(state);
+  	
+  	fetch(this.url, {
+  		method:'POST',
+  		headers:{
+  			'Accept':'application/json',
+  			'Content-Type':'application/json'
+  		},
+  		body:JSON.stringify({
+  			item:texto
+  		})
+  	})
+  		.then((r)=>r.json())
+  		.then((json)=>{
+  			alert('Item inserido com sucesso!');
+  			this.loadLista();
+  		})
+  }
+
 
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.addArea}>
+          <Text style={styles.addTxt}>Adicione uma nova tarefa</Text>
+        	<TextInput style={styles.input} onChangeText={(text) => {
+            let state = this.state;
+            state.input = text;
+            this.setState(state);
+          }} value={this.state.input} />
+          <Button title="Adicionar" onPress={this.addButton} />
+        </View>
         <FlatList
           data={this.state.lista}
           renderItem={({item})=> <Item data={item} />}
@@ -35,10 +81,20 @@ export default class TodoOnline extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
   },
+  addArea: {
+  	marginBottom: 20,
+  	backgroundColor: '#DDDDDD'
+  },
+  input: {
+  	marginTop: 20
+
+  },
+  addTxt:{
+  	fontSize: 14,
+  	textAlign: 'center',
+  	marginBottom: 10
+  }
 
 });
